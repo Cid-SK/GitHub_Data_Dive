db56f9b97c96d7312ecf7a9fc5aeb4941521c6b2
SATHISH KUMAR <157153125+Cid-SK@users.noreply.github.com>
2024-10-12 11:03:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
 from streamlit_option_menu import option_menu
 # from sqlalchemy import create_engine
 # import pymysql
